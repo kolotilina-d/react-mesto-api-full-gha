4ff410d1525c97cb33187a997d7e3179b44c4447
Darya Kolotilina <colotilina.d@yandex.ru>
2023-08-28 12:07:09 +0200

    'add crash-test'

--- backend/app.js
+++ backend/app.js
@@ -21,6 +21,13 @@ app.use(helmet());
 
 app.use(express.json());
 app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use(router);
 app.use(errorLogger);
 app.use(errors());
